039e82425b8a77145f021b3673a1d8962d2f003c
haidarhj <91267549+haidarhj@users.noreply.github.com>
2021-09-30 13:01:46 +0700

    Delete Praktikum 2 directory

--- Praktikum 2/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-	    System.out.println("Nama:Haidar Humam Jaisyurrahiem");
-		System.out.println("NIM:6706210113");
-		System.out.println("Kelas:D3RPLA-45-05");
-    }
-}
